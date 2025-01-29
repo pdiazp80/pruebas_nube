96e430ff0110c0cf26dc749a6bde254dfe804555
Publio Díaz Paez <p.diazp@uniandes.edu.co>
2025-01-29 18:13:15 -0500

    pruebas

--- usuarios/app.py
+++ usuarios/app.py
@@ -9,6 +9,9 @@ app.config['SQLALCHEMY_DATABASE_URI'] = db_config['uri']
 db = SQLAlchemy(app)
 CORS(app)
 
+
+
+
 class User(db.Model):
     __tablename__ = "users"
     id = db.Column(db.Integer, primary_key=True)
